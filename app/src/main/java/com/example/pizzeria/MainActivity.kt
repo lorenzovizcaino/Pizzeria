0472bb5f9f502d1ca797fb851d892aa1c0e4feda
Antonio Lorenzo Vizcaino <lorenzovizcaino@gmail.com>
2023-10-30 21:52:51 +0100

    20231030_21:52

--- app/src/main/java/com/example/pizzeria/MainActivity.kt
+++ app/src/main/java/com/example/pizzeria/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.pizzeria
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import android.widget.Toast
 import com.example.pizzeria.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +16,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         var entrega: String
+
+        binding.radiogroup.setOnCheckedChangeListener  { _,claveloqueyoquiera ->
+            //2 parametros:  1: _ =null en java 2: lo que yo quiera "claveloqueyoquiera" para referenciarlo despues en el when
+            when (claveloqueyoquiera){
+                R.id.domicilio -> Toast.makeText(this,"Entrega a Domicilio", Toast.LENGTH_SHORT).show()
+                R.id.local -> Toast.makeText(this,"Recoger en local",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.jamonserrano.setOnClickListener { onCheckboxClicked(it) }
+
+
+
 
         binding.botonCalcular.setOnClickListener {
             val lista = ArrayList<String>()
@@ -54,6 +70,13 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
 
 
+        }
+    }
+
+    private fun onCheckboxClicked(view: View) {
+        val checked=(view as CheckBox).isChecked
+        when (view.id){
+            R.id.anchoas -> Toast.makeText(this,"Anchoas seleccionado", Toast.LENGTH_SHORT).show()
         }
     }
 }
