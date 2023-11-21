9fb3bc77d4c610fe82e2f36a9edf7ba820741276
Antonio Lorenzo <lorenzovizcaino@gmail.com>
2023-11-21 01:20:19 +0100

    21/11/2023_01:20

--- app/src/main/java/com/example/pizzeria/MainActivity.kt
+++ app/src/main/java/com/example/pizzeria/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.pizzeria
 
 import android.content.Intent
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
