26e21ad244a8828496e033a4d994848fbffb9bf2
lorenzovizcaino <lorenzovizcaino@gmail.com>
2023-11-28 11:11:47 +0100

    28/11/23_11:11

--- app/src/main/java/com/example/pizzeria/SegundaActivityPizzeria.kt
+++ app/src/main/java/com/example/pizzeria/SegundaActivityPizzeria.kt
@@ -6,7 +6,14 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.Context
 import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
+import android.view.Menu
+import android.view.MenuItem
+import android.webkit.URLUtil
 
 class SegundaActivityPizzeria : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,4 +61,60 @@ class SegundaActivityPizzeria : AppCompatActivity() {
         val alertDialog = alertDialogBuilder.create()
         alertDialog.show()
     }
+
+    //menu desde aqui
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menupizzeria, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(item.itemId==R.id.acerca_de){
+            showToast(getString(R.string.autor))
+        }
+        if(item.itemId==R.id.pizza_hut){
+            openWebPage(this,"https://www.pizzahut.es/")
+        }
+        if(item.itemId==R.id.inicio){
+            val intent= Intent(this,InicioActivity::class.java)
+            startActivity(intent)
+        }
+        if(item.itemId==R.id.compartir){
+            showToast(getString(R.string.compartir))
+
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.texto_enviar))
+                type = "text/plain"
+            }
+
+            try {
+
+                startActivity(sendIntent)
+            } catch (e: ActivityNotFoundException) {
+                showToast(getString(R.string.no_se_puede))
+
+            }
+
+        }
+        return true
+    }
+
+    fun openWebPage(context: Context, url: String?) {
+        try {
+            if (!URLUtil.isValidUrl(url)) {
+                showToast(getString(R.string.enlace_no_valido))
+
+            } else {
+                val intent = Intent (Intent.ACTION_VIEW, Uri.parse(url));
+                context.startActivity(intent)
+
+            }
+        } catch (e: ActivityNotFoundException) {
+            showToast(getString(R.string.no_hay_navegador))
+        }
+    }
+    //menu hasta aqui
+
 }
